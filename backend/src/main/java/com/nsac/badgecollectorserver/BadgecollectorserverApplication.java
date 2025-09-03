5976e9fae49bd87414edba0787c04a3fe0ce49a2
Nicholas Sacayan - mac <nksacayan@gmail.com>
2025-09-02 18:11:59 -0700

    Adds sprint boot initializer to backend

---
+++ backend/src/main/java/com/nsac/badgecollectorserver/BadgecollectorserverApplication.java
@@ -0,0 +1,13 @@
+package com.nsac.badgecollectorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadgecollectorserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadgecollectorserverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nsac/badgecollectorserver/BadgecollectorserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsac.badgecollectorserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadgecollectorserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
