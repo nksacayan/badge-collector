cbd6d989cacd2416b461925d8c9196035444ce73
Nicholas Sacayan <nksacayan@gmail.com>
2025-09-03 19:56:43 -0700

    adds baby servlet

--- backend/src/main/java/com/nsac/badgecollectorserver/AppController.java
+++ backend/src/main/java/com/nsac/badgecollectorserver/AppController.java
@@ -1,8 +1,12 @@
 package com.nsac.badgecollectorserver;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
-
+	@GetMapping("/user")
+	public User user() {
+		return new User(0, "nick", new int[] {1, 2, 3});
+	}
 }
