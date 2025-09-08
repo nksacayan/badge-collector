c73484a92ceba069a38d74733ecda2215ad3c38c
Nicholas Sacayan <nksacayan@gmail.com>
2025-09-07 17:11:56 -0700

    Adds controller endpoints

--- backend/src/main/java/com/nsac/badgecollectorserver/AppController.java
+++ backend/src/main/java/com/nsac/badgecollectorserver/AppController.java
@@ -3,15 +3,19 @@ package com.nsac.badgecollectorserver;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class AppController {
 
     private final UserRepository userRepository;
+    private final BadgeRepository badgeRepository;
 
-    public AppController(UserRepository userRepository) {
+    public AppController(UserRepository userRepository, BadgeRepository badgeRepository) {
         this.userRepository = userRepository;
+        this.badgeRepository = badgeRepository;
     }
 
     @GetMapping("/users")
@@ -20,14 +24,38 @@ public class AppController {
     }
 
     // user exists
+    @GetMapping("/user/exists")
+    public Boolean userExists(@RequestParam String name) {
+        return userRepository.userExists(name);
+    }
 
     // register user
+    @GetMapping("/user/create")
+    public User createUser(@RequestParam String name) {
+        return userRepository.createUser(name);
+    }
 
     // login user
+    @GetMapping("/user/login")
+    public User loginUser(@RequestParam String name) {
+        return userRepository.getUser(name);
+    }
 
     // add badge to user
+    @GetMapping("/user/addBadge")
+    public User addBadgeToUser(@RequestParam int userId, @RequestParam int badgeId) {
+        return userRepository.addBadgeToUser(userId, badgeId);
+    }
 
     // get all badges
+    @GetMapping("/badges")
+    public List<Badge> getAllBadges() {
+        return badgeRepository.findAll();
+    }
 
     // get badge
+    @GetMapping("/badge")
+    public List<Badge> getBadge(@RequestParam int id) {
+        return badgeRepository.getBadge(id);
+    }
 }
