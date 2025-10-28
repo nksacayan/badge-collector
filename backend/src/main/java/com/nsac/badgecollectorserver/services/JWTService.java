97c78857bfc11e76ced07d9eca8b13e1555f9ff0
Nicholas Sacayan <nksacayan@gmail.com>
2025-10-28 11:13:49 -0700

    removes validators for simplicity

--- backend/src/main/java/com/nsac/badgecollectorserver/services/JWTService.java
+++ backend/src/main/java/com/nsac/badgecollectorserver/services/JWTService.java
@@ -27,7 +27,6 @@ public class JWTService {
         return Jwts.builder()
             .setSubject(userDTO.getName())
             .setIssuedAt(new Date())
-            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 24 hours
             .signWith(key, SignatureAlgorithm.HS256)
             .compact();
     }
@@ -39,21 +38,5 @@ public class JWTService {
             .parseClaimsJws(token)
             .getBody()
             .getSubject();
-    }
-
-    public boolean isTokenExpired(String token) {
-        Date expiration = Jwts.parserBuilder()
-            .setSigningKey(key)
-            .build()
-            .parseClaimsJws(token)
-            .getBody()
-            .getExpiration();
-
-        return expiration.before(new Date());
-    }
-
-    public boolean validateToken(String token, UserDTO userDTO) {
-        final String username = extractTokenSubject(token);
-        return (username.equals(userDTO.getName()) && !isTokenExpired(token));
     }
 }
