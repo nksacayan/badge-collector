4c36ff5f2862306271ee79b1c7ac707ee3ebfc11
Nicholas Sacayan <nksacayan@gmail.com>
2025-10-28 13:11:46 -0700

    Fixes badge gallery not using name

--- frontend/src/pages/BadgeGallery.jsx
+++ frontend/src/pages/BadgeGallery.jsx
@@ -2,11 +2,17 @@ import { Link } from 'react-router';
 import BadgeCard from '../components/BadgeCard';
 import './BadgeGallery.css';
 import { mockBadges } from '../data/mockBadges';
+import { useContext } from "react";
+import { UserContext } from "../components/UserContext";
+
 
 const BadgeGallery = () => {
+	const context = useContext(UserContext);
+	const { user, setUser } = context;
+
 	return (
 		<div className="gallery-container">
-			<h2 className="badge-welcome">WELCOME, Nick</h2>
+			<h2 className="badge-welcome">WELCOME, {user.name.toUpperCase()}</h2>
 			<h3 className="badge-header">YOUR BADGES</h3>
 
 			<Link to="/leaderboard" className="leaderboard-button common-button">Leaderboard &gt;</Link>
